8f511bb67e4fbbfd75a7f172f20799af9899b6e3
Ronald Cando <ronald.cando.jr@ing.com>
2024-06-18 20:16:44 +0800

    fixed view and error handling

--- src/main/java/com/cando/utilities/services/impl/JsonPrettyPrinterServiceImpl.java
+++ src/main/java/com/cando/utilities/services/impl/JsonPrettyPrinterServiceImpl.java
@@ -11,13 +11,21 @@ package com.cando.utilities.services.impl;
 import com.cando.utilities.services.JsonPrettyPrinterService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JsonPrettyPrinterServiceImpl implements JsonPrettyPrinterService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(JsonPrettyPrinterServiceImpl.class);
+  private StringWriter sw = new StringWriter();
+  private PrintWriter pw = new PrintWriter(sw);
 
   public String printJsonObject(String json) throws JsonProcessingException {
     ObjectMapper objectMapper = new ObjectMapper();
@@ -28,7 +36,11 @@ public class JsonPrettyPrinterServiceImpl implements JsonPrettyPrinterService {
     try {
       new JSONObject(json);
     } catch (JSONException e) {
-      return e.getLocalizedMessage();
+      e.printStackTrace(pw);
+      LOGGER.info("Invalid JSON: {}", e.getMessage());
+      LOGGER.error(sw.getBuffer().toString());
+
+      return sw.getBuffer().toString();
     }
     return "valid";
   }
@@ -37,7 +49,11 @@ public class JsonPrettyPrinterServiceImpl implements JsonPrettyPrinterService {
     try {
       new JSONArray(json);
     } catch (JSONException e) {
-      return e.getMessage();
+      e.printStackTrace(pw);
+      LOGGER.info("Invalid JSON Array: {}", e.getMessage());
+      LOGGER.error(sw.getBuffer().toString());
+
+      return sw.getBuffer().toString();
     }
     return "valid";
   }
