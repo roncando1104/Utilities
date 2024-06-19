ed7db59c76658fa25d648a7489e2049cd1709b3b
Ronald Cando <ronald.cando.jr@ing.com>
2024-06-19 19:55:06 +0800

    fixed view

--- src/main/java/com/cando/utilities/exception/FileUploadException.java
+++ src/main/java/com/cando/utilities/exception/FileUploadException.java
@@ -18,9 +18,9 @@ public class FileUploadException {
 
   @ExceptionHandler(MaxUploadSizeExceededException.class)
   public String handleMaxSizeException(Model model, MaxUploadSizeExceededException e) {
-    model.addAttribute("message", "File is too large!");
+    model.addAttribute("errorMessage", "File is too large!");
 
-    return "upload_form";
+    return "error_page";
   }
 
 }
